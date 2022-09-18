c75d315705a86bae03cc3cb13ccb3ac1ba1d9fa8
Henry Agustin <79470506+henry-1227@users.noreply.github.com>
2022-09-17 18:03:39 -0600

    ejercicio 2 completado

---
+++ ejercicio-2/index.js
@@ -0,0 +1,13 @@
+const nombre = "Henry Alexander Agustin Pérez"
+const edad = 20
+const eresDesarrollador = true
+const fechaNacimiento = new Date(2001, 11, 27)
+const libroFav = {
+    nombre: "padre rico, padre pobre",
+    autor: "Robert Kiyosaki",
+    annyo: 1997,
+    url: "https://www.amazon.com/-/es/Robert-T-Kiyosaki-ebook/dp/B007HPS120",
+}
+
+const lista = [nombre, edad, eresDesarrollador, fechaNacimiento, libroFav]
+console.log(lista);
