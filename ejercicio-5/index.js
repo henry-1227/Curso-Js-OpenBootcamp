79cec30797656781a6177bd50998fd2b2a4f0828
Henry Agustin <79470506+henry-1227@users.noreply.github.com>
2022-11-09 23:17:42 -0600

    Ejercicio 5 Done!

---
+++ ejercicio-5/index.js
@@ -0,0 +1,12 @@
+let alturaCm = 163
+
+let alturaM = 1.63
+
+let pesoKG = 61.23
+
+let alturaRedondeada = Math.ceil(alturaM) 
+console.log(alturaRedondeada);
+
+let pesoKgRedondeado = Math.floor(pesoKG)
+console.log(pesoKgRedondeado);
+
