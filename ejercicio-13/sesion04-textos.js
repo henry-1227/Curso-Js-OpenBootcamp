7a20af7c14d136ec2c2992c3aed781b9ced57a2e
Henry Agustin <79470506+henry-1227@users.noreply.github.com>
2022-11-17 16:46:36 -0600

    Yupi

---
+++ ejercicio-13/sesion04-textos.js
@@ -0,0 +1,11 @@
+// Sesión 4 - Strings (cadenas de caracteres)
+let str_sng = "Hola soy un texto con comillas simples"
+let str_dbl = "Hola soy un texto con comillas dobles"
+let str_3 = "Hola soy un texto con backticks"
+
+console.log(str_sng)
+console.log(str_dbl)
+console.log(str_3)
+
+//npm run lint
+//npm run lint-fix
